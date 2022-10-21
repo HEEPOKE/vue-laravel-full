8d16c6b726403bb148019e550716d26621222f61
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-21 18:38:13 +0700

    rest

--- Frontend/src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <NavbarVue />
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-</template>
-
-<script setup>
-import NavbarVue from "./components/navbar/index.vue";
-</script>
