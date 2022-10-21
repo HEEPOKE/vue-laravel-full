6adc753e4ecc52a9a54cbce97622d45f966275a8
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-21 17:42:43 +0700

    setup

---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <NavbarVue />
+</template>
+
+<script setup>
+import NavbarVue from "../components/navbar/Navbar.vue";
+</script>
+
+<script>
+export default {
+  name: "HomeVue",
+};
+</script>
