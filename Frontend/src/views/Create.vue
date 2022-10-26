030ef0f0de67b2daf8c1a0f1acf79237a3c6ad3a
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-10-26 17:29:27 +0700

    update create

--- Frontend/src/views/Create.vue
+++ Frontend/src/views/Create.vue
@@ -1,13 +1,16 @@
 <template>
+    <NavbarMenu />
     <indexVue />
 </template>
 
 <script>
+import NavbarMenu from "../components/Navbar/Navbar.vue";
 import indexVue from '../components/create/index.vue';
 
 export default {
 name: 'CreateVue',
 components: {
+    NavbarMenu,
         indexVue,
     }
 };
